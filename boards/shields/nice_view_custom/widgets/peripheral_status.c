b8707e6e89c13f2083d5e3b4a11c34490ef79975
PowerAbuser <mamannazir@gmail.com>
2025-11-28 22:53:31 +0100

    img changed

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -36,6 +36,7 @@ LV_IMG_DECLARE(corro10);
 LV_IMG_DECLARE(corro11);
 LV_IMG_DECLARE(corro12);
 LV_IMG_DECLARE(tears_1bit_art);
+LV_IMG_DECLARE(bitlady_rotated);
 
 const lv_img_dsc_t *anim_imgs[] = {
     &corro01,
@@ -141,7 +142,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     lv_obj_t *art = lv_img_create(widget->obj);
-    lv_img_set_src(art, &tears_1bit_art);
+    lv_img_set_src(art, &bitlady_rotated);
 
     // lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
     // lv_obj_center(art);                                         //<--
